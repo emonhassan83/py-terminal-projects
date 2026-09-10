d8b05515314d4dda38b7198b74323d7f3ffd6d0b
emonhassan83 <emonhasan7650@gmail.com>
2026-09-10 13:57:07 +0600

    Add initial implementation of restaurant management system; create FoodItem, Menu, Order, Restaurant, and User classes

---
+++ restarant_management -2/food_item.py
@@ -0,0 +1,8 @@
+class FoodItem:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def __str__(self):
+        return f"{self.name:<20} | ${self.price:<10.2f}"
